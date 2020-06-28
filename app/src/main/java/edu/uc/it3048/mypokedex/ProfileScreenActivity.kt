3e29215f864500705ae0cb6df55fdd2cba2da39e
palmertt <palmertt@mail.uc.edu>
2020-06-28 11:47:28 -0400

    Added camera integration (not permissions), added saved photo to imageView

--- app/src/main/java/edu/uc/it3048/mypokedex/ProfileScreenActivity.kt
+++ app/src/main/java/edu/uc/it3048/mypokedex/ProfileScreenActivity.kt
@@ -1,12 +1,15 @@
 package edu.uc.it3048.mypokedex
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.ImageButton
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.profile_screen_activity.*
+import java.util.jar.Manifest
 
 class ProfileScreenActivity : AppCompatActivity() {
 
@@ -16,7 +19,7 @@ class ProfileScreenActivity : AppCompatActivity() {
 
         // Calling the savedLocations and openCamera method
         savedLocations()
-        openCamera()
+        takePhoto()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -30,7 +33,7 @@ class ProfileScreenActivity : AppCompatActivity() {
     }
 
     // Method to open camera and take photo
-    private fun openCamera(){
+    private fun takePhoto(){
         btnCamera.setOnClickListener {
             val imageIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(imageIntent, 123)
