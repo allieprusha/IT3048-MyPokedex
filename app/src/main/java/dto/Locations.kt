c7b0f564e9130e14a02e9c2f3579e28412302225
palmertt <palmertt@mail.uc.edu>
2020-06-30 20:36:34 -0400

    Added location object class, added minor save button functionality

---
+++ app/src/main/java/dto/Locations.kt
@@ -0,0 +1,4 @@
+package app.plantdiary.individualassignment3048q.dto
+
+data class Locations(var pokemonName: String = "", var pokemonType: String = "", var pokemonDescription: String = "") {
+}
--- app/src/main/java/edu/uc/it3048/mypokedex/ProfileScreenActivity.kt
+++ app/src/main/java/edu/uc/it3048/mypokedex/ProfileScreenActivity.kt
@@ -1,20 +1,18 @@
 package edu.uc.it3048.mypokedex
 
-import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
 import android.widget.ImageButton
 import android.widget.Toast
-import androidx.core.content.ContextCompat
+import app.plantdiary.individualassignment3048q.dto.Locations
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.profile_screen_activity.*
-import java.util.jar.Manifest
 
 class ProfileScreenActivity : AppCompatActivity() {
 
@@ -32,10 +30,11 @@ class ProfileScreenActivity : AppCompatActivity() {
         )
 
         // Calling various methods
-        savedLocations()
+        savedLocationsFolder()
         takePhoto()
         showSignInOptions()
         onSupportNavigateUp()
+        btnSaveLocation()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -68,7 +67,7 @@ class ProfileScreenActivity : AppCompatActivity() {
     }
 
     // Method to view saved pokemon sighting locations
-    private fun savedLocations(){
+    private fun savedLocationsFolder(){
         val folderButton = findViewById<ImageButton>(R.id.imgBtnFolder)
             folderButton.setOnClickListener {
                 val savedLocationsIntent = Intent(this, SavedSightingsActivity::class.java)
@@ -80,5 +79,23 @@ class ProfileScreenActivity : AppCompatActivity() {
     private fun showSignInOptions(){
         startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(loginProviders)
             .setTheme(R.style.AppTheme).build(), LOGIN_REQUEST_CODE)
+    }
+
+    // Button for saving locations to Firebase
+    private fun btnSaveLocation(){
+        val save = findViewById<ImageButton>(R.id.imgBtnSave)
+            save.setOnClickListener {
+                saveLocation()
+            }
+    }
+
+    private fun saveLocation(){
+        var sightingLocation = Locations().apply {
+            pokemonName = edtTextPokemonName.text.toString()
+            pokemonType = edtTextPokemonType.text.toString()
+            pokemonDescription = edtTxtPokemonDescription.text.toString()
+
+            // TODO save this object
+        }
     }
 }
