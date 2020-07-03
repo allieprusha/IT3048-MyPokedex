2abe197b8198835b5fecba8f0809c160fa63e7d4
Benjamin Romig <romigbl@mail.uc.edu>
2020-07-03 16:56:34 -0400

    Housekeeping changes
    Removed unused imports. Changed recyclerview width to match parent to get padding on width. Created a color for the background color. Colors should be hardcoded in the colors.xml.  Applied const to BASE_URL. Reformatted commenting to look nicer and use conventions.

--- app/src/main/java/dao/IPokemonDAO.kt
+++ app/src/main/java/dao/IPokemonDAO.kt
@@ -4,7 +4,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-
 interface IPokemonDAO {
     @GET(value = "https://pokeapi.co/api/v2/pokemon/")
     fun getAllPokemon(): Call<ArrayList<Pokemon>>
