ab85ab44224a1fd14fafb05d36cb916e236bdc64
palmertt <palmertt@mail.uc.edu>
2020-07-19 14:41:42 -0400

    fixed pokemon class

--- app/src/main/java/dto/Pokemon.kt
+++ app/src/main/java/dto/Pokemon.kt
@@ -3,39 +3,22 @@ package dto
 
 import com.google.gson.annotations.SerializedName
 
-data class Pokemon(
-    @SerializedName("avg_spawns")
-    val avgSpawns: Int,
-    @SerializedName("candy")
-    val candy: String,
-    @SerializedName("candy_count")
-    val candyCount: Int,
-    @SerializedName("egg")
-    val egg: String,
-    @SerializedName("height")
-    val height: String,
-    @SerializedName("id")
-    val id: Int,
-    @SerializedName("img")
-    val img: String,
-    @SerializedName("multipliers")
-    val multipliers: List<Double>,
-    @SerializedName("name")
-    val name: String,
-    @SerializedName("next_evolution")
-    val nextEvolution: List<NextEvolution>,
-    @SerializedName("num")
-    val num: String,
-    @SerializedName("prev_evolution")
-    val prevEvolution: List<PrevEvolution>,
-    @SerializedName("spawn_chance")
-    val spawnChance: Double,
-    @SerializedName("spawn_time")
-    val spawnTime: String,
-    @SerializedName("type")
-    val type: List<String>,
-    @SerializedName("weaknesses")
-    val weaknesses: List<String>,
-    @SerializedName("weight")
-    val weight: String
-)
+class Pokemon {
+
+    var id = 0
+    var num : String? = null
+    var name : String? = null
+    var img : String? = null
+    var type : List<String>? = null
+    var height: String? = null
+    var weight : String? = null
+    var candy : String? = null
+    var egg : String? = null
+    var spawn_choice = 0.toDouble()
+    var avg_spawns = 0.toDouble()
+    var spawn_time : String? = null
+    var multipliers : List<Double>? = null
+    var weakness : List<String>? = null
+    var next_evolution : List<Evolution>? = null
+    var prev_evolution : List<Evolution>? = null
+}
