210eb8a4e5a6aa5f68f5a85d36c6ae6487727e74
palmertt <palmertt@mail.uc.edu>
2020-07-27 19:44:04 -0400

    Can now see some firestore data. Added FirestoreRecyclerAdapter

--- app/src/main/java/dto/Locations.kt
+++ app/src/main/java/dto/Locations.kt
@@ -3,7 +3,4 @@ package dto
 import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 
-data class Locations(var pokemonName: String = "",
-                     var pokemonType: String = "",
-                     var pokemonDescription: String = ""
-)
+data class Locations(var pokemonName: String = "", var pokemonType: String = "", var pokemonDescription: String = "")
--- app/src/main/java/edu/uc/it3048/mypokedex/SavedSightingsActivity.kt
+++ app/src/main/java/edu/uc/it3048/mypokedex/SavedSightingsActivity.kt
@@ -3,18 +3,31 @@ package edu.uc.it3048.mypokedex
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import android.widget.ListView
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.ktx.Firebase
 import dto.LocationAdapter
+import dto.Locations
+import kotlinx.android.synthetic.main.location_row.*
 import kotlinx.android.synthetic.main.saved_sightings_activity.*
 
 class SavedSightingsActivity : AppCompatActivity() {
 
+    val firebaseReference = FirebaseFirestore.getInstance()
+    val query = firebaseReference.collection("locations").orderBy("pokemonName", Query.Direction.ASCENDING)
+    val options = FirestoreRecyclerOptions.Builder<Locations>().setQuery(query, Locations::class.java).build()
+    private var adapter: LocationsFirestoreRecyclerAdapter? = null
 
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +40,10 @@ class SavedSightingsActivity : AppCompatActivity() {
         actionBar.setDisplayHomeAsUpEnabled(true)
 
         recViewLocations.layoutManager = LinearLayoutManager(this)
-        recViewLocations.adapter = LocationAdapter()
+        //recViewLocations.adapter = LocationAdapter()
+
+        adapter = LocationsFirestoreRecyclerAdapter(options)
+        recViewLocations.adapter = adapter
     }
 
     // Press the back button to go back to previous activity
@@ -35,5 +51,49 @@ class SavedSightingsActivity : AppCompatActivity() {
 
         onBackPressed()
         return true
+    }
+
+    private inner class LocationsViewHolder internal constructor(private val view: View) : RecyclerView.ViewHolder(view) {
+        internal fun setPokemonName(pokemonName: String) {
+            val txtPokemonName = view.findViewById<TextView>(R.id.txtName)
+            txtPokemonName.text = pokemonName
+        }
+
+        internal fun setPokemonDescription(pokemonDescription: String) {
+            val txtPokemonDescription = view.findViewById<TextView>(R.id.txtDescription)
+            txtPokemonDescription.text = pokemonDescription
+        }
+
+        internal fun setPokemonType(pokemonType: String) {
+            val txtPokemonType = view.findViewById<TextView>(R.id.txtType)
+            txtPokemonType.text = pokemonType
+        }
+    }
+
+    private inner class LocationsFirestoreRecyclerAdapter internal constructor(options: FirestoreRecyclerOptions<Locations>) : FirestoreRecyclerAdapter<Locations, LocationsViewHolder>(options) {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LocationsViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.location_row, parent, false)
+            return LocationsViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: LocationsViewHolder, position: Int, model: Locations) {
+            holder.setPokemonName(model.pokemonName)
+            holder.setPokemonDescription(model.pokemonDescription)
+            holder.setPokemonType(model.pokemonType)
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter!!.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (adapter != null) {
+            adapter!!.stopListening()
+        }
     }
 }
