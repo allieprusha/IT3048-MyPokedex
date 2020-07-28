a09c04f91673ab64766b5f977885f27fd3ce74e8
palmertt <palmertt@mail.uc.edu>
2020-07-28 18:59:46 -0400

    Wrong branch but spinner dropdown works way better now.

--- app/src/main/java/edu/uc/it3048/mypokedex/MainActivity.kt
+++ app/src/main/java/edu/uc/it3048/mypokedex/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.uc.it3048.mypokedex
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     private var compositeDisposable = CompositeDisposable()
     private val retrofit = RetrofitClientInstance.retrofitInstance
     private var iPokemonList = retrofit?.create(IPokemonDAO::class.java)
-    var pokemonList : List<Pokemon> = ArrayList()
+    var pokemonList : MutableList<Pokemon> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,12 +46,19 @@ class MainActivity : AppCompatActivity() {
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { pokemon ->
-                pokemonList = pokemon.pokemon!!
+                pokemonList = pokemon.pokemon!! as MutableList<Pokemon>
+
+                val newPokemonList = ArrayList<String>()
+                newPokemonList.add(0, "All Pokemon")
+                for (pokemonName in pokemonList) {
+                    newPokemonList.add(pokemonName.pokemonName!!)
+                }
+
 
                 val pokemonViewAdapter = PokemonViewAdapter(this, pokemonList)
                 rcycViewPokemon.adapter = pokemonViewAdapter
 
-                val autoCompleteAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, pokemonList)
+                val autoCompleteAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, newPokemonList)
                 pokemonSpinner.adapter = autoCompleteAdapter
 
                 pokemonSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -58,17 +66,23 @@ class MainActivity : AppCompatActivity() {
 
                     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
 
-                        val pokemonInfoIntent = Intent(view?.context, PokemonInfoActivity::class.java)
-                        pokemonInfoIntent.putExtra("image", pokemonList[position].pokemonImg)
-                        pokemonInfoIntent.putExtra("id", pokemonList[position].pokemonId.toString())
-                        pokemonInfoIntent.putExtra("num", pokemonList[position].pokemonNum.toString())
-                        pokemonInfoIntent.putExtra("type", pokemonList[position].pokemonType.toString())
-                        pokemonInfoIntent.putExtra("candy", pokemonList[position].pokemonCandy)
-                        pokemonInfoIntent.putExtra("egg", pokemonList[position].pokemonEgg)
-                        pokemonInfoIntent.putExtra("spawn_choice", pokemonList[position].pokemonSpawnChoice.toString())
-                        pokemonInfoIntent.putExtra("avg_spawns", pokemonList[position].pokemonAvgSpawns.toString())
-                        pokemonInfoIntent.putExtra("spawn_time", pokemonList[position].pokemonSpawnTime)
-                        view?.context?.startActivity(pokemonInfoIntent)
+                        if (autoCompleteAdapter.getItem(position) == "All Pokemon") {
+                            Log.d("AutoComplete", "All Pokemon Called")
+                        } else {
+
+                            val pokemonInfoIntent = Intent(view?.context, PokemonInfoActivity::class.java)
+                            pokemonInfoIntent.putExtra("image", pokemonList[position - 1].pokemonImg)
+                            pokemonInfoIntent.putExtra("id", pokemonList[position - 1].pokemonId.toString())
+                            pokemonInfoIntent.putExtra("num", pokemonList[position - 1].pokemonNum.toString())
+                            pokemonInfoIntent.putExtra("type", pokemonList[position - 1].pokemonType.toString())
+                            pokemonInfoIntent.putExtra("candy", pokemonList[position - 1].pokemonCandy)
+                            pokemonInfoIntent.putExtra("egg", pokemonList[position - 1].pokemonEgg)
+                            pokemonInfoIntent.putExtra("spawn_choice", pokemonList[position - 1].pokemonSpawnChoice.toString())
+                            pokemonInfoIntent.putExtra("avg_spawns", pokemonList[position - 1].pokemonAvgSpawns.toString())
+                            pokemonInfoIntent.putExtra("spawn_time", pokemonList[position - 1].pokemonSpawnTime)
+                            view?.context?.startActivity(pokemonInfoIntent)
+                        }
+
                     }
                 }
             })
